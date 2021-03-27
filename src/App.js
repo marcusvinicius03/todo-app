c387d354e5e4c0daf077421f2e38fe1b80bc14ae
Marcus Vinicius <marcusvncsg17@gmail.com>
2021-03-27 04:01:39 -0300

    varias coisas feitas ao mesmo tempo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const newTodoInput = useRef(null)
+
+  function handleNewTodo (e) {
+    e.preventDefault()
+    
+    if(newTodoInput.current.value === '') {
+      return
+    }
+
+    let value = newTodoInput.current.value
+
+    setTodos([
+      ...todos, {
+        id: todos.length,
+        title: value
+      }
+    ])
+
+    newTodoInput.current.value = ''
+  }
+
+  function deleteTodo (id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
   return (
     <div className="App">
-      Basic template
+      <header id="header">
+        <div id="header-items-group">
+          <input type="text" placeholder="Adicionar nova tarefa..." id="add-task-input" ref={newTodoInput} />
+          <button id="add-task-button" onClick={e => handleNewTodo(e)}>Adicionar à lista</button>
+        </div>
+      </header>
+
+      <div id="body-wrapper">
+      {todos.length == 0 ? 
+            <span id="no-task-message">Você não tem nenhum a fazer...</span>
+          : 
+        <div id="tasks-wrapper">
+          {todos.length == 0 ? 
+            <span>Você não tem nenhum a fazer...</span>
+          : todos.map(todo => (
+            <div key={todo.id} className="todo-container">
+            <div id="todo-left">
+              <label>
+                <input type="checkbox" />
+                <span>Concluída?</span>
+              </label>
+            <span className="todo-title">{todo.title}</span>
+            </div>
+            <button className="delete-task" onClick={() => deleteTodo(todo.id)}>Deletar</button>
+            </div>
+            ))}
+          </div>
+          }
+      </div>
     </div>
   );
 }
